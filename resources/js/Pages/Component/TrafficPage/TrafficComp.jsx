ce8356956ac6080dfb9d7015a4766a274c6b3871
ahmadbsat153 <ahmadb@gtls.com.au>
2024-09-11 08:49:24 +0300

    Pagination of the Traffic Table

--- resources/js/Pages/Component/TrafficPage/TrafficComp.jsx
+++ resources/js/Pages/Component/TrafficPage/TrafficComp.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 
-function TrafficComp() {
-  return (
-    <div>TrafficComp</div>
-  )
+import ReactDataGrid from '@inovua/reactdatagrid-community'
+import '@inovua/reactdatagrid-community/index.css'
+const gridStyle = { minHeight: 550, marginTop: 10 };
+
+const columns = [
+  { name: 'id', type: 'number', maxWidth: 40, header: 'ID', defaultVisible: false },
+  { name: 'firstName', defaultFlex: 2, header: 'First Name' },
+  { name: 'lastName', defaultFlex: 2, header: 'Last Name' },
+  { name: 'email', defaultFlex: 3, header: 'Email' }
+];
+
+const loadData = ({ skip, limit, sortInfo }) => {
+  const url =
+    `https://demos.reactdatagrid.io/api/v1/contacts` +
+    '?skip=' +
+    skip +
+    '&limit=' +
+    limit +
+    '&sortInfo=' +
+    JSON.stringify(sortInfo);
+
+  return fetch(url).then(response => {
+      const totalCount = response.headers.get('X-Total-Count');
+      return response.json().then(data => {
+        return Promise.resolve({ data, count: parseInt(totalCount) });
+      })
+    })
 }
 
-export default TrafficComp
+const TrafficComp = () => {
+  const dataSource = useCallback(loadData, []);
+
+  return (
+    <div>
+      <h3>Remote data and pagination example</h3>
+      <ReactDataGrid
+        idProperty="id"
+        style={gridStyle}
+        columns={columns}
+        pagination
+        dataSource={dataSource}
+        defaultLimit={10}
+      />
+    </div>
+  );
+}
+
+export default () => <TrafficComp />
