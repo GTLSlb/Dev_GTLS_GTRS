9aee16c04751d9ac406a4c2b75a65122eed704b1
ahmadbsat153 <ahmadb@gtls.com.au>
2024-09-24 15:55:39 +0300

    Testing

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="locale" property= "og:locale" content="en_US">
     <meta name="type" property= "og:type" content="website">
-    <meta name="url" property="og:url" content="https://web.gtls.au" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="url" property="og:url" content="https://web.gtls.store" />
     <meta name="theme-color" content="#317EFB"/>
     <meta name="site_name" property="og:site_name" content="GTLS" />
     <meta name="image" property="og:image" content="{{ asset('favicon.ico') }}" />
