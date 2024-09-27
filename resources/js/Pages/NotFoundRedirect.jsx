f154249781fe15d40ba647ce5745cec3bdbbdbc1
MiraCh-gtls <mirac@gtls.com.au>
2024-09-27 14:04:52 +0300

    remove comment

--- resources/js/Pages/NotFoundRedirect.jsx
+++ resources/js/Pages/NotFoundRedirect.jsx
@@ -4,10 +4,7 @@ import { Route } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
 
 export default function NotFoundRedirect() {
-    // const navigate = useNavigation();
-    // useEffect(() => {
-    //     navigate("/");
-    // },[])
+
     return (
         <div className="h-screen w-screen flex justify-center">
             <Navigate to="/notFound" />
