21879795fabca0ec2ee84b0e02d9669ff9b2f70d
bassame <143480472+bassameGtls@users.noreply.github.com>
2024-12-06 13:11:57 +0200

    fix: KPI packs

--- resources/js/Pages/Component/Presentation/GraphPresentation.jsx
+++ resources/js/Pages/Component/Presentation/GraphPresentation.jsx
@@ -40,28 +40,6 @@ const MultiChartLine = ({ url, currentUser, AToken }) => {
                     </h1>
                 </div>
             </div>
-            <ul className="flex space-x-0 mt-5">
-                <li
-                    className={`cursor-pointer ${
-                        activeComponentIndex === 0
-                            ? "text-dark border-b-4 py-2 border-goldt font-bold text-xs sm:text-base"
-                            : "text-dark py-2 text-xs sm:text-base border-b-2 border-gray-300"
-                    }`}
-                    onClick={() => handleItemClick(0)}
-                >
-                    <div className="px-2"> Report 1 </div>
-                </li>
-                <li
-                    className={`cursor-pointer ${
-                        activeComponentIndex === 1
-                            ? "text-dark border-b-4 py-2 border-goldt font-bold text-xs sm:text-base"
-                            : "text-dark py-2 text-xs sm:text-base border-b-2 border-gray-300"
-                    }`}
-                    onClick={() => handleItemClick(1)}
-                >
-                    <div className="px-2"> Report 2 </div>
-                </li>
-            </ul>
             <div className="grid grid-cols-2 gap-4">
                 {/* <BarGraph />
             <GroupedBar /> */}
