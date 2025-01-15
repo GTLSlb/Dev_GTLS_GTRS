045cde3e664217ab1e049fc239b2387e9eccbeed
ahmadbsat153 <ahmadb@gtls.com.au>
2025-01-15 12:05:15 +0200

    Fix: Export incase there was no value in ConsReference

--- resources/js/Pages/Component/GtrsCons.jsx
+++ resources/js/Pages/Component/GtrsCons.jsx
@@ -456,9 +456,13 @@ export default function GtrsCons({
                             acc[column] = "";
                         }
                     } else if (columnKey === "ConsReferences") {
-                        acc[column] = person[columnKey]
-                            .map((item) => item.Value)
-                            .join(", ");
+                        if (Array.isArray(person[columnKey])) {
+                            acc[column] = person[columnKey]
+                                .map((item) => item.Value)
+                                .join(", ");
+                        } else {
+                            acc[column] = ""; // Fallback for undefined or non-array values
+                        }
                     } else if (fieldsToCheck.includes(columnKey)) {
                         acc[column] = isDummyAccount(person[columnKey]);
                     } else {
@@ -794,7 +798,9 @@ export default function GtrsCons({
             getFilterValue: ({ data }) => {
                 if (data.ConsReferences && data.ConsReferences.length > 0) {
                     // Join all reference values into a single string for filtering
-                    return data.ConsReferences.map(ref => ref.Value).join(", ");
+                    return data.ConsReferences.map((ref) => ref.Value).join(
+                        ", "
+                    );
                 }
                 return "";
             },
@@ -867,7 +873,7 @@ export default function GtrsCons({
                         leaveFrom="opacity-100 translate-y-0"
                         leaveTo="opacity-0 translate-y-1"
                     >
-                        <Popover.Panel className="absolute left-20 lg:left-0 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
+                        <Popover.Panel className="absolute left-20 lg:left-0 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2">
                             <div className=" max-w-md flex-auto overflow-hidden rounded-lg bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
                                 <div className="p-4">
                                     <div className="mt-2 flex flex-col">
