bf37b6c3296271dea2e5ec885c960dc75f41d1fc
ahmadbsat153 <ahmadb@gtls.com.au>
2025-03-25 14:22:28 +0200

    fix : Made sure that the loading of the contact Report is correct

--- resources/js/Pages/Component/ContactsRep/ContactRep.jsx
+++ resources/js/Pages/Component/ContactsRep/ContactRep.jsx
@@ -170,7 +170,26 @@ function ContactRep({ currentUser, AToken, url }) {
         );
     }, [columns, contactsData, filtersValue, setFiltersValue]);
 
-    return loading ? AnimatedLoading() : renderTable();
+    return loading ? (
+        <div className="min-h-screen md:pl-20 pt-16 h-full flex flex-col items-center justify-center">
+            <div className="flex items-center justify-center">
+                <div
+                    className={`h-5 w-5 bg-goldd rounded-full mr-5 animate-bounce`}
+                ></div>
+                <div
+                    className={`h-5 w-5 bg-goldd rounded-full mr-5 animate-bounce200`}
+                ></div>
+                <div
+                    className={`h-5 w-5 bg-goldd rounded-full animate-bounce400`}
+                ></div>
+            </div>
+            <div className="text-dark mt-4 font-bold">
+                Please wait while we get the data for you.
+            </div>
+        </div>
+    ) : (
+        renderTable()
+    );
 }
 
 export default ContactRep;
