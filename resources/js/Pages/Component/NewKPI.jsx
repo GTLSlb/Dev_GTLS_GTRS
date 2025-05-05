114e8f3a3b69d07ec4262de1659597be6155ee75
GTLSlb <123725092+GTLSlb@users.noreply.github.com>
2025-05-05 10:21:34 +0300

    Update NewKPI.jsx

--- resources/js/Pages/Component/NewKPI.jsx
+++ resources/js/Pages/Component/NewKPI.jsx
@@ -1335,7 +1335,6 @@ function NewKPI({
                                     <button
                                         className={`inline-flex items-center justify-center w-[10rem] h-[36px] rounded-md border bg-gray-800 px-4 py-2 text-xs font-medium leading-4 text-white shadow-sm hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2`}
                                         disabled={
-                                            filteredData?.length === 0 ||
                                             loading
                                         }
                                         onClick={() => CalculateKPI()}
