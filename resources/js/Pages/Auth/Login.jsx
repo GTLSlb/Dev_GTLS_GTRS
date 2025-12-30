a2042e734d737bbb7acd6bc000e8170694970c64
MiraCh-gtls <mirac@gtls.com.au>
2025-12-30 12:51:16 +0200

    Chore:Update "gtls-npm-libraries" from 1.2.23 to 1.2.24

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -3,6 +3,11 @@ import Logo from "../../assets/pictures/Logo-upscaled.png";
 import "../../../css/scroll.css";
 import { LoginPage } from "gtls-npm-libraries";
 import { clearMSALLocalStorage, pca } from "@/CommonFunctions";
+import {
+    GoogleReCaptchaProvider,
+    useGoogleReCaptcha,
+    GoogleReCaptcha,
+} from "react-google-recaptcha-v3";
 
 export default function Login() {
     const gtamURl = window.Laravel.gtamUrl;
@@ -14,25 +19,34 @@ export default function Login() {
         clearMSALLocalStorage();
     }, []);
 
+    const [recaptchaValue, setRecaptchaValue] = React.useState("");
     return (
         <div className="h-screen w-full">
-        <LoginPage
-            appDomain={appDomain}
-            googlekey={googleKey}
-            redirectURL="/gtrs/"
-            loginURL="/loginComp"
-            gtamURl={gtamURl}
-            pca={pca}
-            canResetPassword={true}
-            handleForgotPassword={() =>
-                (window.location.href = "/forgot-password")
-            }
-            microsoftURL="/microsoftToken"
-            backToHomeURL={backToHomeURL}
-            gtlsLogo={Logo}
-            redirectUrl={window.Laravel.azureCallback}
-            isTest={window.Laravel.isTest || false}
-        />
-    </div>
+            <GoogleReCaptchaProvider reCaptchaKey={googleKey}>
+                <LoginPage
+                    appDomain={appDomain}
+                    googlekey={googleKey}
+                    redirectURL="/gtrs/"
+                    loginURL="/loginComp"
+                    gtamURl={gtamURl}
+                    pca={pca}
+                    canResetPassword={true}
+                    handleForgotPassword={() =>
+                        (window.location.href = "/forgot-password")
+                    }
+                    microsoftURL="/microsoftToken"
+                    backToHomeURL={backToHomeURL}
+                    gtlsLogo={Logo}
+                    redirectUrl={window.Laravel.azureCallback}
+                    isTest={window.Laravel.isTest || false}
+                    recaptchaValue={recaptchaValue}
+                />
+                <GoogleReCaptcha
+                    onVerify={(token) => {
+                        setRecaptchaValue(token);
+                    }}
+                />
+            </GoogleReCaptchaProvider>
+        </div>
     );
 }
